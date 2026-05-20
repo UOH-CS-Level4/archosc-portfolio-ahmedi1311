ee930383a386d2776256091f93674c9cc2484365
Ahmed Hussien <ah422759@gmail.com>
2026-05-20 08:30:28 +0100

    Final portfolio submission

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function showMessage() {
 		alert("Welcome to my AOSC Portfolio");
 }
+
+const darkModeButton = document.getElementById("darkModeBtn");
+
+if(darkModeButton){
+
+    darkModeButton.addEventListener("click", function(){
+
+        document.body.classList.toggle("dark-mode");
+
+    });
+
+}
